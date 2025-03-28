9c00b18f3edb103b9f18f95cbdb37f3ec8b7bb9d
Gustavo Araujo Santos <gustavoaraujo600@hotmail.com>
2025-03-28 15:59:20 -0300

    Create service

---
+++ SwiftService/Sources/main.swift
@@ -0,0 +1,45 @@
+import Foundation
+import Vapor
+import OpenAPIRuntime
+import OpenAPIVapor
+
+
+// Define a type that conforms to the generated protocol.
+struct GreetingServiceAPIImpl: APIProtocol {
+    func getEmoji(
+        _ input: Operations.GetEmoji.Input
+    ) async throws -> Operations.GetEmoji.Output {
+        let emojis = "👋👍👏🙏🤙🤘"
+        let emoji = String(emojis.randomElement()!)
+        return .ok(.init(body: .plainText(.init(emoji))))
+    }
+    
+    func getGreeting(
+        _ input: Operations.GetGreeting.Input
+    ) async throws -> Operations.GetGreeting.Output {
+        let name = input.query.name ?? "Stranger"
+        let greeting = Components.Schemas.Greeting(message: "Hello, \(name)!")
+        return .ok(.init(body: .json(greeting)))
+    }
+}
+
+// Create your Vapor application.
+let app = try await Vapor.Application.make()
+
+
+// Create a VaporTransport using your application.
+let transport = VaporTransport(routesBuilder: app)
+
+
+// Create an instance of your handler type that conforms the generated protocol
+// defining your service API.
+let handler = GreetingServiceAPIImpl()
+
+
+// Call the generated function on your implementation to add its request
+// handlers to the app.
+try handler.registerHandlers(on: transport, serverURL: Servers.Server1.url())
+
+
+// Start the app as you would normally.
+try await app.execute()
