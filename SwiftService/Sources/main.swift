7f773a34594f2292bae0330233aab28a65396114
Gustavo Araujo Santos <gustavoaraujo600@hotmail.com>
2025-03-31 13:10:35 -0300

    Remove unnecessary spaces

--- SwiftService/Sources/main.swift
+++ SwiftService/Sources/main.swift
@@ -3,7 +3,6 @@ import Vapor
 import OpenAPIRuntime
 import OpenAPIVapor
 
-
 // Define a type that conforms to the generated protocol.
 struct GreetingServiceAPIImpl: APIProtocol {
     func getEmoji(
@@ -26,20 +25,16 @@ struct GreetingServiceAPIImpl: APIProtocol {
 // Create your Vapor application.
 let app = try await Vapor.Application.make()
 
-
 // Create a VaporTransport using your application.
 let transport = VaporTransport(routesBuilder: app)
-
 
 // Create an instance of your handler type that conforms the generated protocol
 // defining your service API.
 let handler = GreetingServiceAPIImpl()
 
-
 // Call the generated function on your implementation to add its request
 // handlers to the app.
 try handler.registerHandlers(on: transport, serverURL: Servers.Server1.url())
-
 
 // Start the app as you would normally.
 try await app.execute()
